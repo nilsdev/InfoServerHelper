79b15e0e052751a59878e3aee4942c0d95c31fc8
42LoCo42 <11213kbm@gmail.com>
2021-01-23 20:16:56 +0100

    Vergleiche mit Rollen vom Server

--- main.py
+++ main.py
@@ -1,48 +1,31 @@
 #!/usr/bin/env python3
 
-import os
 import discord
-from discord.utils import get
 from discord.ext import commands
 import asyncio
 
-TOKEN = 'NjU1NDI0MTczMjMzMTQzODA5.XfT5VA.nR3-oRlVbyGjOdmobNWRH6tv81k'
-GUILD = '772795652458938388'
+token_file = open("token", "r")
+TOKEN = token_file.read()
 
 bot = commands.Bot(command_prefix='+')
 
-rollen = [
-        "smib", 
-        "smsb",
-        "eti_fachschaft",
-        "elektrotechnik", 
-        "maschinenbau",
-        "regenerative_energien",
-        "wirtschaftsinfo",
-        "motorsport_engineering"
-        "campus",
-        "games",
-        "linux",
-        "sund-xplosion",
-        "mathe_lernen"
-        ] 
-
 @bot.command(name='rollen')
+async def _role(ctx, mode, role):
+    opt_guild = ctx.guild
+    if opt_guild == None:
+        print("no guild")
+        return
 
-async def _role(ctx, mode, role: discord.Role):
-
-    mode = mode.lower()
-    role = role.lower()
-    
-
+    roles = opt_guild.roles
     run = 0
-    for x in rollen:
-        if str(role).lower() in str(x).lower(): 
+    for r in roles:
+        if r.name.lower() == role.lower():
+            role = r
             run = 1
+            break
 
-    if run == 1: 
-
-        if mode == 'remove': 
+    if run == 1:
+        if mode == 'remove':
             await ctx.author.remove_roles(role)
             print("%s hat kein %s mehr" % (ctx.author, role))
 
@@ -50,8 +33,7 @@ async def _role(ctx, mode, role: discord.Role):
             await ctx.author.dm_channel.send(
                 'Du hast nicht mehr die Rolle `%s` \nDu kannst sie mit `+rollen add %s` wieder hinzufügen' % (role, role)
             )
-
-        if mode == 'add': 
+        elif mode == 'add':
             await ctx.author.add_roles(role)
             print("%s hat jetzt %s" % (ctx.author, role))
 
@@ -59,9 +41,7 @@ async def _role(ctx, mode, role: discord.Role):
             await ctx.author.dm_channel.send(
                 'Du hast jetzt die Rolle `%s` \nDu kannst sie mit `+rollen remove %s` entfernen' % (role, role)
             )
-
         run = 0
-
     else:
         await ctx.author.create_dm()
         if str(role) == 'mod' or str(role) == "admin":
